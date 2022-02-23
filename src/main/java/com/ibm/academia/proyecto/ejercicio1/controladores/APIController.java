702867b3f915d21b1dbf76d3881c303d03795af9
LuisAHC <luishceciliano@gmail.com>
2022-02-22 22:24:37 -0600

    maneja excepciones

--- src/main/java/com/ibm/academia/proyecto/ejercicio1/controladores/APIController.java
+++ src/main/java/com/ibm/academia/proyecto/ejercicio1/controladores/APIController.java
@@ -1,5 +1,7 @@
 package com.ibm.academia.proyecto.ejercicio1.controladores;
 
+import com.ibm.academia.proyecto.ejercicio1.excepciones.BadRequestException;
+import com.ibm.academia.proyecto.ejercicio1.excepciones.NotFoundException;
 import com.ibm.academia.proyecto.ejercicio1.modelos.entidades.TarjetaCredito;
 import com.ibm.academia.proyecto.ejercicio1.servicios.TarjetaCreditoDAO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,33 +34,25 @@ public class APIController {
     public ResponseEntity<?> obtenerTarjetasDisponibles(@PathVariable(value = "pasion") String pasion,
                                                         @PathVariable(value = "salario") Double salario,
                                                         @PathVariable(value = "edad") Integer edad){
-        try {
-            List<TarjetaCredito> lTarjetasSalario = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorSalario(salario);
-            List<TarjetaCredito> lTarjetasEdad = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorEdad(edad);
-            List<TarjetaCredito> lTarjetasPasion = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorPasion(pasion);
+        List<TarjetaCredito> lTarjetasSalario = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorSalario(salario);
+        List<TarjetaCredito> lTarjetasEdad = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorEdad(edad);
+        List<TarjetaCredito> lTarjetasPasion = (List<TarjetaCredito>) tarjetaCreditoDao.buscarTarjetasPorPasion(pasion);
 
-            List<TarjetaCredito> tarjetasAuxiliar = new ArrayList<>();
+        List<TarjetaCredito> tarjetasAuxiliar = new ArrayList<>();
 
-            for (TarjetaCredito tarjeta : lTarjetasSalario) {
-                if(lTarjetasEdad.contains(tarjeta)) {
+        for (TarjetaCredito tarjeta : lTarjetasSalario) {
+            if(lTarjetasEdad.contains(tarjeta)) {
                     tarjetasAuxiliar.add(tarjeta);
-                }
             }
-
-            List<TarjetaCredito> tarjetasDisponibles = new ArrayList<>();
-            for (TarjetaCredito tarjeta : lTarjetasPasion) {
-                if (tarjetasAuxiliar.contains(tarjeta))
-                    tarjetasDisponibles.add(tarjeta);
-            }
-
-            if (tarjetasDisponibles.isEmpty())
-                return new ResponseEntity<>("No existen tarjetas disponibles para este perfil", HttpStatus.OK);
-
-            return new ResponseEntity<>(tarjetasDisponibles, HttpStatus.OK);
-
-        }catch (RuntimeException e){
-            return new ResponseEntity<>("Encontre un error", HttpStatus.BAD_REQUEST);
         }
-    }
 
+        List<TarjetaCredito> tarjetasDisponibles = new ArrayList<>();
+        for (TarjetaCredito tarjeta : lTarjetasPasion) {
+            if (tarjetasAuxiliar.contains(tarjeta))
+                tarjetasDisponibles.add(tarjeta);
+        }
+        if (tarjetasDisponibles.isEmpty())
+            throw new NotFoundException("No existen tarjetas disponibles para este perfil");
+        return new ResponseEntity<>(tarjetasDisponibles, HttpStatus.OK);
+    }
 }
