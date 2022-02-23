a5b1958b08d80e01eadeccfea311103bcd5a23cb
LuisAHC <luishceciliano@gmail.com>
2022-02-22 22:08:27 -0600

    Lista clase para rellenar base de datos

--- src/main/java/com/ibm/academia/proyecto/ejercicio1/CreadorDatos.java
+++ src/main/java/com/ibm/academia/proyecto/ejercicio1/CreadorDatos.java
@@ -31,6 +31,67 @@ public class CreadorDatos implements CommandLineRunner {
                 15000.00, 24, 75, "luisceciliano");
         tarjetaCreditoDao.guardar(tarjetaCredito3);
 
+        TarjetaCredito tarjetaCredito4 = new TarjetaCredito(null, "Office Depot", 7000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito4);
+
+        TarjetaCredito tarjetaCredito5 = new TarjetaCredito(null, "Costco", 7000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito5);
+
+        TarjetaCredito tarjetaCredito6 = new TarjetaCredito(null, "Best Buy Banamex", 7000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito6);
+
+        TarjetaCredito tarjetaCredito7 = new TarjetaCredito(null, "The Home Depot", 7000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito7);
+
+        TarjetaCredito tarjetaCredito8 = new TarjetaCredito(null, "Platinum", 30000.00,
+                null, 23, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito8);
+
+        TarjetaCredito tarjetaCredito9 = new TarjetaCredito(null, "Citi / AAdvantage", 12000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito9);
+
+        TarjetaCredito tarjetaCredito10 = new TarjetaCredito(null, "Banamex Teleton", 12000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito10);
+
+        TarjetaCredito tarjetaCredito11 = new TarjetaCredito(null, "APAC", 12000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito11);
+
+        TarjetaCredito tarjetaCredito12 = new TarjetaCredito(null, "Martí Clasica Citibanamex", 7000.00,
+                34999.00, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito12);
+
+        TarjetaCredito tarjetaCredito13 = new TarjetaCredito(null, "América Deporteísmo", 7000.00,
+                34999.00, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito13);
+
+        TarjetaCredito tarjetaCredito14 = new TarjetaCredito(null, "Pumas Deporteísmo", 7000.00,
+                34999.00, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito14);
+
+        TarjetaCredito tarjetaCredito15 = new TarjetaCredito(null, "Toluca Deporteísmo", 7000.00,
+                34999.00, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito15);
+
+        TarjetaCredito tarjetaCredito16 = new TarjetaCredito(null, "La Verde Deporteísmo", 7000.00,
+                34999.00, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito16);
+
+        TarjetaCredito tarjetaCredito17 = new TarjetaCredito(null, "Martí Premium Citibanamex", 35000.00,
+                null, 18, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito17);
+
+        TarjetaCredito tarjetaCredito18 = new TarjetaCredito(null, "Oro", 15000.00,
+                29999.00, 24, 75, "luisceciliano");
+        tarjetaCreditoDao.guardar(tarjetaCredito18);*/
+
+        /*
         Pasion pasion1 = new Pasion(null, "Shopping", "luisceciliano");
         pasionDao.guardar(pasion1);
         Pasion pasion2 = new Pasion(null, "Travels", "luisceciliano");
@@ -44,7 +105,32 @@ public class CreadorDatos implements CommandLineRunner {
         Pasion pasion6 = new Pasion(null, "My style", "luisceciliano");
         pasionDao.guardar(pasion6);*/
 
-        /*Long[] pasionesId = {1L, 6L};
-        tarjetaCreditoDao.asociarTarjetaPasion(1L, pasionesId);*/
+        /*
+        Long[] pasionesId = {1L, 6L};
+        tarjetaCreditoDao.asociarTarjetaPasion(1L, pasionesId);
+        tarjetaCreditoDao.asociarTarjetaPasion(2L, pasionesId);
+        Long[] pasionesId2 = {1L};
+        tarjetaCreditoDao.asociarTarjetaPasion(3L, pasionesId2);
+        Long[] pasionesId3 = {1L, 4L};
+        tarjetaCreditoDao.asociarTarjetaPasion(4L, pasionesId3);
+        tarjetaCreditoDao.asociarTarjetaPasion(5L, pasionesId3);
+        tarjetaCreditoDao.asociarTarjetaPasion(6L, pasionesId3);
+        tarjetaCreditoDao.asociarTarjetaPasion(7L, pasionesId3);
+        Long[] pasionesId4 = {1L, 2L, 6L};
+        tarjetaCreditoDao.asociarTarjetaPasion(8L, pasionesId4);
+        Long[] pasionesId5 = {2L};
+        tarjetaCreditoDao.asociarTarjetaPasion(9L, pasionesId5);
+        Long[] pasionesId6 = {3L};
+        tarjetaCreditoDao.asociarTarjetaPasion(10L, pasionesId6);
+        tarjetaCreditoDao.asociarTarjetaPasion(11L, pasionesId6);
+        Long[] pasionesId7 = {5L};
+        tarjetaCreditoDao.asociarTarjetaPasion(12L, pasionesId7);
+        tarjetaCreditoDao.asociarTarjetaPasion(13L, pasionesId7);
+        tarjetaCreditoDao.asociarTarjetaPasion(14L, pasionesId7);
+        tarjetaCreditoDao.asociarTarjetaPasion(15L, pasionesId7);
+        tarjetaCreditoDao.asociarTarjetaPasion(16L, pasionesId7);
+        tarjetaCreditoDao.asociarTarjetaPasion(17L, pasionesId7);
+        Long[] pasionesId8 = {4L};
+        tarjetaCreditoDao.asociarTarjetaPasion(18L, pasionesId8);*/
     }
 }
